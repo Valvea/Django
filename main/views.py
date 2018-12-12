81497db8306319333319bbda957fe5cb8f993985
valle <taytds10@gmail.com>
2018-12-13 00:24:33 +0300

    Cделал приложение

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_view(request):
+    return render(request,'main/index.html')
+
+def contats_view(request):
+    return render(request,'main/contacts.html')
+
+def about_view(request):
+    return render(request,'main/about.html')
