437ed998f12b5587f5b2381913976d589917bdc4
valle <taytds10@gmail.com>
2018-12-20 00:44:40 +0300

    Добавлена база даннных, изменены ссылки на теги, добавлены модели

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=250)
+    description = models.TextField()
+    short_description = models.TextField()
